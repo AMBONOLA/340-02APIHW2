8e78fefb7d6f87a3486ea1ebc1327aada28acc1a
Andy <andreabonola@gmail.com>
2023-02-04 23:33:39 -0500

    Add files via upload

---
+++ HW2RestAPI/src/test/java/com/HW2/HW2RestAPI/Hw2RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW2.HW2RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
